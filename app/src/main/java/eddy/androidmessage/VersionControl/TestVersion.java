28f811070471ad3f1756fcce6894d2521a8f1524
fazzirrullah <eddy.fazzy@gmail.com>
2016-08-24 19:05:54 +0800

    Version Control

---
+++ app/src/main/java/eddy/androidmessage/VersionControl/TestVersion.java
@@ -0,0 +1,13 @@
+package eddy.androidmessage.VersionControl;
+
+/**
+ * Created by fazzirrullah on 24/8/2016.
+ */
+public class TestVersion {
+
+    public String version = "v2";
+
+    public String getVersion() {
+        return version;
+    }
+}
